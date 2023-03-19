e93b03a7f3fb084d7ce9d19592e9c806631db8b1
Mogician <464734297@qq.com>
2023-03-19 15:39:00 +0800

    update edge

--- src/Index.java
+++ src/Index.java
@@ -5,16 +5,16 @@ import java.util.*;
 
 public class Index {
     private static final int K = 100;
-    Map<String, List<IndexEntry>> indexes, revIndexes; //每个点的正反索引
+    Map<String, Map<String, IndexEntry>> indexes, revIndexes; //每个点的正反索引
     Set<String> coverNodes; //点覆盖集合
 
     void Init(List<Edge> l) { //初始化
         coverNodes = new HashSet<String>();
 
         GenerateCover(l); //生成点覆盖
-        indexes = new HashMap<String, List<IndexEntry>>();
+        indexes = new HashMap<String, Map<String, IndexEntry>>();
         GenerateIndexes(l, indexes); //生成索引
-        revIndexes = new HashMap<String, List<IndexEntry>>();
+        revIndexes = new HashMap<String, Map<String, IndexEntry>>();
         List<Edge> revEdges = new LinkedList<Edge>();
         for (Edge e : l) {
             revEdges.add(new Edge(e.v, e.u, e.prob)); //建反向图
@@ -31,10 +31,10 @@ public class Index {
         }
     }
 
-    void GenerateIndexes(List<Edge> l, Map<String, List<IndexEntry>> index) {
+    void GenerateIndexes(List<Edge> l, Map<String, Map<String, IndexEntry>> index) {
         for (Edge e : l) {
-            index.computeIfAbsent(e.u, k -> new LinkedList<IndexEntry>()); //初始化
-            index.computeIfAbsent(e.v, k -> new LinkedList<IndexEntry>());
+            index.computeIfAbsent(e.u, k -> new HashMap<String, IndexEntry>()); //初始化
+            index.computeIfAbsent(e.v, k -> new HashMap<String, IndexEntry>());
         }
         Map<String, Set<Edge>> mp = new HashMap<String, Set<Edge>>(); //每个点连出哪些边
         for (Edge e : l) {
@@ -43,8 +43,8 @@ public class Index {
             mp.computeIfAbsent(e.v, k -> new HashSet<Edge>());
         }
 
-        for (Map.Entry<String, List<IndexEntry>> entry : index.entrySet()) {
-            entry.getValue().add(new IndexEntry(entry.getKey(), 0, 1)); //自己到自己
+        for (Map.Entry<String, Map<String, IndexEntry>> entry : index.entrySet()) {
+            entry.getValue().put(entry.getKey(),new IndexEntry(entry.getKey(), 0, 1)); //自己到自己
         }
 
         Map<String, Integer> counter = new HashMap<String, Integer>();
@@ -64,7 +64,7 @@ public class Index {
                         q.offer(out.v);
                         dis.put(out.v, dis.get(now) + 1);
                         if (coverNodes.contains(out.v)) { //只计算到其他点覆盖中的点
-                            index.get(node).add(new IndexEntry(out.v, dis.get(out.v), 0));
+                            index.get(node).put(out.v, new IndexEntry(out.v, dis.get(out.v), 0));
                         }
                     }
                 }
@@ -90,19 +90,19 @@ public class Index {
                 }
             }
             //计算可达概率
-            for (IndexEntry entry : index.get(node)) {
-                if (!entry.v.equals(node) && counter.containsKey(entry.v)) {
-                    entry.prob = (double) counter.get(entry.v) / K;
+            for (Map.Entry<String, IndexEntry> entry : index.get(node).entrySet()) {
+                if (!entry.getKey().equals(node) && counter.containsKey(entry.getKey())) {
+                    entry.getValue().prob = (double) counter.get(entry.getKey()) / K;
                 }
             }
         }
         //计算不在点覆盖中的点概率，只算直接有边与其相连的点覆盖中的点
-        for (Map.Entry<String, List<IndexEntry>> entry : index.entrySet()) {
+        for (Map.Entry<String, Map<String, IndexEntry>> entry : index.entrySet()) {
             String node = entry.getKey();
             if (!coverNodes.contains(node)) {
                 for (Edge out : mp.get(node)) {
                     if (coverNodes.contains(out.v)) {
-                        entry.getValue().add(new IndexEntry(out.v, 1, out.prob));
+                        entry.getValue().put(out.v,new IndexEntry(out.v, 1, out.prob));
                     }
                 }
             }
@@ -111,42 +111,32 @@ public class Index {
 
     int QueryDis(String u, String v) { //询问最短路
         if (coverNodes.contains(u) && coverNodes.contains(v)) { //都在点覆盖中
-            for (IndexEntry out : indexes.get(u)) {
-                if (out.v.equals(v)) {
-                    return out.dis;
-                }
-            }
+            if (indexes.get(u).containsKey(v)) return indexes.get(u).get(v).dis;
+            return -1;
         } else if (!coverNodes.contains(u) && coverNodes.contains(v)) { //u不在v在
             int minDis = -1;
-            for (IndexEntry cv : indexes.get(u)) { //枚举u相邻的点覆盖
-                for (IndexEntry out : indexes.get(cv.v)) {
-                    if (out.v.equals(v)) {
-                        if (minDis == -1 || cv.dis + out.dis < minDis) minDis = cv.dis + out.dis; //取最短路
+            for (IndexEntry cv : indexes.get(u).values()) { //枚举u相邻的点覆盖
+                    if (indexes.get(cv.v).containsKey(v)) {
+                        if (minDis == -1 || cv.dis + indexes.get(cv.v).get(v).dis < minDis) minDis = cv.dis + indexes.get(cv.v).get(v).dis; //取最短路
                     }
-                }
             }
             return minDis;
         } else if (coverNodes.contains(u) && !coverNodes.contains(v)) { //v不在u在
             int minDis = -1;
-            for (IndexEntry cv : revIndexes.get(v)) { //查反向索引
-                for (IndexEntry out : revIndexes.get(cv.v)) {
-                    if (out.v.equals(u)) {
-                        if (minDis == -1 || cv.dis + out.dis < minDis) minDis = cv.dis + out.dis;
+            for (IndexEntry cv : revIndexes.get(v).values()) { //查反向索引
+                    if (revIndexes.get(cv.v).containsKey(u)) {
+                        if (minDis == -1 || cv.dis + revIndexes.get(cv.v).get(u).dis < minDis) minDis = cv.dis + revIndexes.get(cv.v).get(u).dis;
                     }
-                }
             }
             return minDis;
         } else if (!coverNodes.contains(u) && !coverNodes.contains(v)) { //都不在
             int minDis = -1;
-            for (IndexEntry cvu : indexes.get(u)) { //枚举u相邻的点覆盖
-                for (IndexEntry cvv : revIndexes.get(v)) { //枚举v相邻的点覆盖，反向索引
-                    for (IndexEntry out : indexes.get(cvu.v)) {
-                        if (out.v.equals(cvv.v)) {
-                            if (minDis == -1 || cvu.dis + cvv.dis + out.dis < minDis)
-                                minDis = cvu.dis + cvv.dis + out.dis;
-                            ;
+            for (IndexEntry cvu : indexes.get(u).values()) { //枚举u相邻的点覆盖
+                for (IndexEntry cvv : revIndexes.get(v).values()) { //枚举v相邻的点覆盖，反向索引
+                        if (indexes.get(cvu.v).containsKey(cvv.v)) {
+                            if (minDis == -1 || cvu.dis + cvv.dis + indexes.get(cvu.v).get(cvv.v).dis < minDis)
+                                minDis = cvu.dis + cvv.dis + indexes.get(cvu.v).get(cvv.v).dis;
                         }
-                    }
                 }
             }
             return minDis;
@@ -156,41 +146,31 @@ public class Index {
 
     double QueryProb(String u, String v) { //查询最大可达概率
         if (coverNodes.contains(u) && coverNodes.contains(v)) { //都在点覆盖中
-            for (IndexEntry out : indexes.get(u)) {
-                if (out.v.equals(v)) {
-                    return out.prob;
-                }
-            }
+            if (indexes.get(u).containsKey(v)) return indexes.get(u).get(v).prob;
+            return -1;
         } else if (!coverNodes.contains(u) && coverNodes.contains(v)) { //u不在v在
             double maxProb = 0;
-            for (IndexEntry cv : indexes.get(u)) {
-                for (IndexEntry out : indexes.get(cv.v)) {
-                    if (out.v.equals(v)) {
-                        if (cv.prob * out.prob > maxProb) maxProb = cv.prob * out.prob; //取最大概率
+            for (IndexEntry cv : indexes.get(u).values()) {
+                    if (indexes.get(cv.v).containsKey(v)) {
+                        if (cv.prob * indexes.get(u).get(cv.v).prob > maxProb) maxProb = cv.prob * indexes.get(u).get(cv.v).prob; //取最大概率
                     }
-                }
             }
             return maxProb;
         } else if (coverNodes.contains(u) && !coverNodes.contains(v)) { //v不在u在
             double maxProb = 0;
-            for (IndexEntry cv : revIndexes.get(v)) {
-                for (IndexEntry out : revIndexes.get(cv.v)) {
-                    if (out.v.equals(u)) {
-                        if (cv.prob * out.prob > maxProb) maxProb = cv.prob * out.prob;
+            for (IndexEntry cv : revIndexes.get(v).values()) {
+                    if (revIndexes.get(cv.v).containsKey(u)) {
+                        if (cv.prob * revIndexes.get(cv.v).get(u).prob > maxProb) maxProb = cv.prob * revIndexes.get(cv.v).get(u).prob;
                     }
-                }
             }
             return maxProb;
         } else if (!coverNodes.contains(u) && !coverNodes.contains(v)) { //都不在
             double maxProb = 0;
-            for (IndexEntry cvu : indexes.get(u)) {
-                for (IndexEntry cvv : revIndexes.get(v)) {
-                    for (IndexEntry out : indexes.get(cvu.v)) {
-                        if (out.v.equals(cvv.v)) {
-                            if (cvu.prob * cvv.prob * out.prob > maxProb) maxProb = cvu.prob * cvv.prob * out.prob;
-                            ;
+            for (IndexEntry cvu : indexes.get(u).values()) {
+                for (IndexEntry cvv : revIndexes.get(v).values()) {
+                        if (indexes.get(cvu.v).containsKey(cvv.v)) {
+                            if (cvu.prob * cvv.prob * indexes.get(cvu.v).get(cvv.v).prob > maxProb) maxProb = cvu.prob * cvv.prob * indexes.get(cvu.v).get(cvv.v).prob;
                         }
-                    }
                 }
             }
             return maxProb;
@@ -198,8 +178,32 @@ public class Index {
         return 0;
     }
 
+    void Update(Edge e){
+        updateIndex(indexes,revIndexes,e);
+        updateIndex(revIndexes,indexes, new Edge(e.v,e.u,e.prob));
+    }
+    void updateIndex(Map<String, Map<String, IndexEntry>> index,Map<String, Map<String, IndexEntry>> rev, Edge e){
+        if (index.get(e.u).containsKey(e.v)){
+            index.get(e.u).get(e.v).dis=1;
+            index.get(e.u).get(e.v).prob=1-(1-e.prob)*(1-index.get(e.u).get(e.v).prob);
+        }
+        else {
+            index.get(e.u).put(e.v,new IndexEntry(e.v,1,e.prob));
+        }
+        for (IndexEntry out: index.get(e.v).values()){
+            if (index.get(e.u).containsKey(out.v)){
+                index.get(e.u).get(out.v).dis=Math.min(index.get(e.u).get(out.v).dis,out.dis+1);
+                index.get(e.u).get(out.v).prob=Math.max(index.get(e.u).get(out.v).prob,out.prob*index.get(e.u).get(e.v).prob);
+            }
+            if (rev.get(out.v).containsKey(e.u)){
+                rev.get(out.v).get(e.u).dis=Math.min(rev.get(out.v).get(e.u).dis,out.dis+1);
+                rev.get(out.v).get(e.u).prob=Math.max(rev.get(out.v).get(e.u).prob,out.prob*index.get(e.u).get(e.v).prob);
+            }
+        }
+    }
     public static void main(String[] args) throws Exception {
-        File f = new File(".\\dataset\\hep\\graph.txt");
+//        File f = new File(".\\dataset\\hep\\graph.txt");
+        File f = new File(".\\in.txt");
         Scanner s = new Scanner(f);
         List<Edge> l = new LinkedList<Edge>();
         int maxLine = 100;
@@ -222,10 +226,17 @@ public class Index {
         x.Init(l);
         Scanner std = new Scanner(System.in);
         while (true) {
+            int op = std.nextInt();
             int a = std.nextInt();
             int b = std.nextInt();
-            System.out.println(x.QueryDis(Integer.toString(a), Integer.toString(b)));
-            System.out.println(x.QueryProb(Integer.toString(a), Integer.toString(b)));
+            if (op==0){
+                double p=std.nextDouble();
+                x.Update(new Edge(Integer.toString(a),Integer.toString(b),p));
+            }
+            else {
+                System.out.println(x.QueryDis(Integer.toString(a), Integer.toString(b)));
+                System.out.println(x.QueryProb(Integer.toString(a), Integer.toString(b)));
+            }
         }
     }
 }
